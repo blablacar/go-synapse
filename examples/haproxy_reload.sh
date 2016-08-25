60b35459608282ccfe816a41ed1a4f6ca436def1
Arnaud Lemaire <alemaire@norad.fr>
2016-08-25 17:27:18 +0200

    update haproxy reload script

--- examples/haproxy_reload.sh
+++ examples/haproxy_reload.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -x
 
 PID_FILE=/tmp/haproxy.pid
 
-#nl-qdisc-add --dev=lo --parent=1:4 --id=40: --update plug --buffer
+nl-qdisc-add --dev=lo --parent=1:4 --id=40: --update plug --buffer
 /usr/sbin/haproxy -f ${HAP_CONFIG} -p ${PID_FILE} -D  $([ -f ${PID_FILE} ] && echo -sf $(cat ${PID_FILE}))
-#nl-qdisc-add --dev=lo --parent=1:4 --id=40: --update plug --release-indefinit
+fail=$?
+nl-qdisc-add --dev=lo --parent=1:4 --id=40: --update plug --release-indefinit
 
+exit 1
